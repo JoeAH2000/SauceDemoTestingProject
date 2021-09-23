f5c4623bf07f72909d10a0b7e35db87c6342871b
CPlatts <cplatts@spartaglobal.com>
2021-09-23 10:51:08 +0100

    Added username and password validity check

--- src/test/java/com/sparta/sdet/pages/LoginPage.java
+++ src/test/java/com/sparta/sdet/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.sparta.sdet.pages;
 
+import com.sparta.sdet.util.PropertiesLoader;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,9 +9,58 @@ import org.openqa.selenium.support.PageFactory;
 import static com.sparta.sdet.base.TestBase.webDriver;
 
 public class LoginPage {
-    @FindBy(id = "user-name")
-    WebElement userName;
+
+    @FindBy(id="user-name") WebElement userName_txt;
+    @FindBy(id="password") WebElement passWord_txt;
+    @FindBy(id="login-button") WebElement Login_btn;
+
+    private String username;
+    private String password;
+
     public LoginPage() {
         PageFactory.initElements(webDriver, this);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void enterUsername(String username) {
+
+    }
+
+    public boolean isUsernameValid() {
+        return isValid("user-name", this.username);
+    }
+
+    public void enterPassword(String password) {
+
+    }
+
+    public boolean isPasswordValid() {
+        return isValid("password", this.password);
+    }
+
+    private boolean isValid(String propertyKey, String text) {
+        boolean valid = text.equals(
+                PropertiesLoader.getProperties().getProperty(propertyKey));
+
+        return valid;
+    }
+
+    public boolean loginButtonClick() {
+        return false;
     }
 }
