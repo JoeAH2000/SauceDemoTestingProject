a98129fe84b57e5a0f863bdb34aaf6ec2e533fbd
CPlatts <cplatts@spartaglobal.com>
2021-09-24 11:45:02 +0100

    Removed WebDriver being passed in to interface methods as parameter. Methods now use the static webDriver in TestBase

--- src/test/java/com/sparta/sdet/util/Footerable.java
+++ src/test/java/com/sparta/sdet/util/Footerable.java
@@ -3,9 +3,9 @@ package com.sparta.sdet.util;
 import org.openqa.selenium.WebDriver;
 
 public interface Footerable {
-    String testFacebook(WebDriver webDriver);
-    String testTwitter(WebDriver webDriver);
-    String testLinkedin(WebDriver webDriver);
-    String testTermsAndConditions(WebDriver webDriver);
-    String testPrivacyPolicy(WebDriver webDriver);
+    String testFacebook();
+    String testTwitter();
+    String testLinkedin();
+    String testTermsAndConditions();
+    String testPrivacyPolicy();
 }
