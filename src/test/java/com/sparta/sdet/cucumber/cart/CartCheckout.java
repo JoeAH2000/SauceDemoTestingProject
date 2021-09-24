bfa750bf8de5e561bfc6c3449552b8f1a990dd77
CPlatts <cplatts@spartaglobal.com>
2021-09-24 11:07:55 +0100

    Implemented ChartCheckout

--- src/test/java/com/sparta/sdet/cucumber/cart/CartCheckout.java
+++ src/test/java/com/sparta/sdet/cucumber/cart/CartCheckout.java
@@ -1,14 +1,31 @@
 package com.sparta.sdet.cucumber.cart;
 
+import com.sparta.sdet.pages.CartPage;
+import com.sparta.sdet.pages.CheckoutStepOnePage;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
 
 public class CartCheckout {
+
+    private CartPage cartPage;
+    private CheckoutStepOnePage checkoutStepOnePage = null;
+
+    @Before
+    public void setup() {
+        cartPage = new CartPage();
+    }
+
     @When("I click the checkout button")
     public void iClickTheCheckoutButton() {
+
+        checkoutStepOnePage = cartPage.goToCheckout();
+
     }
 
     @Then("I should be taken to the checkout step one page")
     public void iShouldBeTakenToTheCheckoutStepOnePage() {
+        Assertions.assertNotNull(checkoutStepOnePage);
     }
 }
